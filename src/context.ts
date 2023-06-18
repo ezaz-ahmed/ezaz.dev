1364257e85c0e729b820b9fa05c7d765927f601e
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-06-18 14:48:15 +0600

    Clean project

--- src/context.ts
+++
@@ -1,8 +0,0 @@
-import { createContextId, ContextId } from '@builder.io/qwik';
-import type { ThemePreference } from './components/theme-toggle/theme-toggle';
-
-export interface GenericType {
-  ...
-}
-
-export const QwikCityContext = createContextId<GenericType>(name: string): ContextId<GenericType>;
