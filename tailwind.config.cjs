04670d9d43218f3c4a08dfcad516901347e71154
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-13 19:38:10 +0600

    feat: hero section complete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,15 @@ module.exports = {
     fontFamily: {
       sans: ['Matter', ...defaultTheme.fontFamily.sans],
     },
+    primary: "rgb(var(--color-primary) / <alpha-value>)",
+    text: "rgb(var(--color-text) / <alpha-value>)",
+    success: "rgb(var(--color-success) / <alpha-value>)",
+    info: "rgb(var(--color-info) / <alpha-value>)",
+    warn: "rgb(var(--color-warn) / <alpha-value>)",
+    error: "rgb(var(--color-error) / <alpha-value>)",
+    transparent: "transparent",
+    current: "currentColor",
+
     extend: {
       colors: {
         bglight: "#F9FAFB",
