6f707f250b150e7b37c9d21c9c2e7309039cfb46
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-01-14 23:34:25 +0600

    feature: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), sitemap()]
 });
