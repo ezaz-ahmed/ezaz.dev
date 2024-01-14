ed7cc6e81602c6655bc73de0255ab826a8c405e4
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-01-15 00:18:18 +0600

    feature: first step of completing personal blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), sitemap()]
+  site: "https://ezaz.dev",
+  integrations: [icon()],
 });
