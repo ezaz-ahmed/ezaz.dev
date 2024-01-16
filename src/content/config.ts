6a739be3902c457063438173ee7aaafd915fbb73
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-01-16 21:45:47 +0600

    feat: static blog

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z, type CollectionEntry } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
+
+export type BlogType = CollectionEntry<"blog">;
