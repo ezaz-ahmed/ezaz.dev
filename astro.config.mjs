256e0dacffa80a000684e9d5b4d25d66a5476420
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-01-21 00:42:52 +0600

    ⤴ï¸â-m ⚡mo:zap:nvert - (MD to MDX)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ezaz.dev",
-  integrations: [icon()],
+  integrations: [icon(), mdx()]
 });
