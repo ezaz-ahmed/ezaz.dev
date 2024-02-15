48e9bc1d772cfded2047a3a2a217e6feeb1ba8a8
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-02-15 21:15:43 +0600

    :package: update: package upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-import compress from "astro-compress";
 
 export default defineConfig({
   site: "https://ezaz.dev",
   integrations: [
     icon(),
     mdx(),
-    // compress({
-    //   CSS: false,
-    // }),
   ],
 });
