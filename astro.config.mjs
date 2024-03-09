6477f8da9e3200b8631d6ca6f9fd979d95d658d8
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-03-09 23:28:33 +0600

    🚀feat: Add "Launching Soon" poster

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import { remarkReadingTime } from "./src/utils";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://ezaz.dev",
-  integrations: [
-    icon(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime]
-  }
+  integrations: [icon()]
 });
--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const HOMEPAGE_ARTICLE_LIMIT = 4;
