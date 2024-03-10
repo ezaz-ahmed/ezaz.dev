60a3c5dc07a58aaebbee5f1e61c6e8e2e21740cd
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-03-11 00:28:24 +0600

    ✨feat[ui]: Add Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ezaz.dev",
-  integrations: [icon()]
+  integrations: [icon(), tailwind(), react()]
 });
